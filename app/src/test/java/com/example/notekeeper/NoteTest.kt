a449592710250ba20ef27b70a271598735cabe24
José Augusto <jose@aioria.com.br>
2020-02-15 10:40:20 -0300

    noteTest

--- app/src/test/java/com/example/notekeeper/NoteTest.kt
+++ app/src/test/java/com/example/notekeeper/NoteTest.kt
@@ -1,6 +1,7 @@
 package com.example.notekeeper
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
 
@@ -16,5 +17,33 @@ class NoteTest {
         val note2 = Note(id = 1, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP1)
 
         assertEquals(note1, note2)
+    }
+
+
+    // Compare different ids
+    @Test
+    internal fun isNotesEquals_differentIds() {
+        val note1 = Note(id = 1, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP1)
+        val note2 = Note(id = 2, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP1)
+
+        assertNotEquals(note1, note2)
+    }
+
+    // Compare different timestamps
+    @Test
+    internal fun isNotesEquals_differentTimestamps() {
+        val note1 = Note(id = 1, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP1)
+        val note2 = Note(id = 1, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP2)
+
+        assertEquals(note1, note2)
+    }
+
+    // Compare different titles
+    @Test
+    internal fun isNotesEquals_differentTitles() {
+        val note1 = Note(id = 1, title = "Note 1", content = "This is note 1", timestamp = TIMESTAMP1)
+        val note2 = Note(id = 1, title = "Note 2", content = "This is note 1", timestamp = TIMESTAMP1)
+
+        assertNotEquals(note1, note2)
     }
 }
